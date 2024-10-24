bbb2268bae8437e48b63282e060c23a4a1b9fcf2
Praveen <120632172+Praveen12401@users.noreply.github.com>
2024-10-24 04:56:28 +0000

    only property-file commit

--- property-method.py
+++ property-method.py
@@ -1,1 +1,16 @@
-# here property method explain
+# here __call__ method explain
+from typing import Any
+
+
+class add(object):
+    def __init__(self,a,b) -> None:
+        self.a=a
+        self.b=b
+    def __call__(self,a,b, *args: Any, **kwds: Any) -> Any:
+        return a+b
+    
+a1=add(10,20)
+print(callable(a1))
+print(a1(2,2))
+
+
